9121a18e86eefeeb5c590171ebad36f9f81d5af6
remuscl <38448996+remuscl@users.noreply.github.com>
2018-06-22 20:09:24 +0800

    Update t2.c

--- t2.c
+++ t2.c
@@ -1,1 +1,6 @@
-void __attribute__((force_align_arg_pointer)) test(){}
+#include<stdio.h>
+extern int* gli;
+void __attribute__((force_align_arg_pointer)) test(){
+    *gli = *gli + 1;
+    printf("hah:)");
+}
