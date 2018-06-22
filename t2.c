144c07ada10a3b1c491033721562c302ee1ae11e
remuscl <38448996+remuscl@users.noreply.github.com>
2018-06-22 20:49:33 +0800

    Update t2.c

--- t2.c
+++ t2.c
@@ -4,3 +4,13 @@ void __attribute__((force_align_arg_pointer)) test(){
     *gli = *gli + 1;
     printf("hah:)");
 }
+void *
+xcalloc (size_t x, size_t y)
+{
+  size_t sz;
+  if (__builtin_mul_overflow (x, y, &sz))
+    return NULL;
+  void *ret = malloc (sz);
+  if (ret) memset (res, 0, sz);
+  return ret;
+}
